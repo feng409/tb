bba01e5fe7ffb60712cc38e439306aa9e84d9dac
chemf <eoyohe@gmail.com>
2018-08-07 00:08:16 +0800

    [IMP] add parse comment for taobao and tmall
        \               ,-----._
      .  \         .  ,'        `-.__,------._
     //   \      __\\'                        `-.
    ((    _____-'___))                           |
     `:='/     (alf_/                            |
     `.=|      |='                               |
        |)   O |                                  \
        |      |                               /\  \
        |     /                          .    /  \  \
        |    .-..__            ___   .--' \  |\   \  |
       |o o  |     ``--.___.  /   `-'      \  \\   \ |
        `--''        '  .' / /             |  | |   | \
                     |  | / /              |  | |   mmm
                     |  ||  |              | /| |
                     ( .' \ \              || | |
                     | |   \ \            // / /
                     | |    \ \          || |_|
                    /  |    |_/         /_|
                   /__/

--- tb/pipelines.py
+++ tb/pipelines.py
@@ -8,4 +8,5 @@
 
 class TbPipeline(object):
     def process_item(self, item, spider):
+        # print(item['comment_url'])
         return item
--- tb/spiders/comment.py
+++ tb/spiders/comment.py
@@ -5,8 +5,9 @@ from urllib.parse import urlencode
 import json
 import re
 
-URL_COMMENT_TAOBAO = ''
-URL_COMMENT_TMAIL = 'https://rate.tmall.com/list_detail_rate.htm?itemId=%s&sellerId=%s'
+URL_COMMENT_TAOBAO = 'https://rate.taobao.com/feedRateList.htm?auctionNumId={item_id}&userNumId={user_id}&currentPageNum=1&pageSize=20'
+URL_COMMENT_TMALL = 'https://rate.tmall.com/list_detail_rate.htm?itemId={item_id}&sellerId={user_id}'
+
 
 class CommentSpider(scrapy.Spider):
     name = 'comment'
@@ -22,10 +23,19 @@ class CommentSpider(scrapy.Spider):
         for shop in data['mods']['itemlist']['data']['auctions']:
             yield ShopItem(shop)
             # todo 根据不同店面parse
-            yield scrapy.Request()
+            if 'tmall.com' in shop['comment_url']:
+                print(URL_COMMENT_TMALL.format(item_id=shop['nid'], user_id=shop['user_id']))
+                yield scrapy.Request(
+                    url=URL_COMMENT_TMALL.format(item_id=shop['nid'], user_id=shop['user_id']),
+                    callback=self.parse_comment_tmall)
+            # else:
+            #     yield scrapy.Request(
+            #         URL_COMMENT_TAOBAO.format(item_id=shop['nid'], user_id=shop['user_id']),
+            #         callback=self.parse_comment_taobao)
 
-    def parse_comment_taobao(self, response):
+    def parse_comment_tmall(self, response):
         # 真特么有毒的返回，不是标准json的json
+        print(response.text[:20])
         data = json.loads('{%s}' % response.text.strip())
         # 过滤id关键字
         items = []
@@ -33,7 +43,10 @@ class CommentSpider(scrapy.Spider):
             _['_id'] = _['id']
             del _['id']
             items.append(CommentItem(_))
+            print('tmail ->', _['rateContent'], end='\n\n')
         return items
 
-    def parse_comment_tmail(self, response):
-        pass
+    def parse_comment_taobao(self, response):
+        comments = json.loads(response.text.strip()[1:-1])['comments']
+        for comment in comments:
+            print('taobao -> ', comment['content'], end='\n\n')
